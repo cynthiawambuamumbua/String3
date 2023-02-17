5e84fe43be21ebe9964739785cac436e3705ddc6
cynthiawambuamumbua <cynthiawambuamumbua@gmail.com>
2023-02-17 16:15:48 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    var stringtext = "akirachix"
+    println(stringtext[0] +" "+stringtext[2]+" "+stringtext[3])
+
+    var name = "Hi my name is Cynthia Mumbua"
+    var age = "I am 19 years old"
+    println(name + " "+ age)
+
+    var city = "Nairobi"
+    println(city.length)
+
+    var name1 = "Brian"
+    var name2 = "Elizabeth"
+    if(name1.equals(name2)){
+    println("That's me!")}else{
+    println("I don't know who is that")}
+
+}
